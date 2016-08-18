741c5b074696a5c4244d1cc6758326bb47e38af5
sashgorokhov <sashgorokhov@gmail.com>
2016-08-18 15:59:26 +0300

    Reformat

--- utils/downloads.go
+++ utils/downloads.go
@@ -1,25 +1,22 @@
 package utils
 
 import (
-)
-import (
-	"net/http"
 	"errors"
-	"strconv"
-	"io"
-	"os"
 	"github.com/cheggaaa/pb"
+	"io"
+	"net/http"
+	"os"
 	"path"
+	"strconv"
 )
 
-
-func Download_size (url string) (uint64, error) {
+func Download_size(url string) (uint64, error) {
 	resp, err := http.Head(url)
 	if err != nil {
 		return 0, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return 0, errors.New("HEAD "+url+" "+resp.Status)
+		return 0, errors.New("HEAD " + url + " " + resp.Status)
 	}
 	content_length := resp.Header.Get("Content-Length")
 	if content_length == "" {
@@ -34,18 +31,18 @@ func Download_size (url string) (uint64, error) {
 
 type CallbackReader struct {
 	total, done uint64
-	callback func(total, done uint64)
-	reader io.Reader
+	callback    func(total, done uint64)
+	reader      io.Reader
 }
 
 func (r *CallbackReader) Read(p []byte) (int, error) {
 	n, err := r.reader.Read(p)
-    	r.done += uint64(n)
+	r.done += uint64(n)
 
-    	if err == nil && r.callback != nil {
+	if err == nil && r.callback != nil {
 		r.callback(r.total, r.done)
-    	}
-    	return n, err
+	}
+	return n, err
 }
 
 func Download_file(url string, filename string) error {
